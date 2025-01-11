29af2933dd96ee66ee112581d18d0fdd87d05137
Nast <111@mail.ru>
2025-01-11 20:54:57 +0100

    added functions

--- Makefile
+++ Makefile
@@ -1,9 +1,19 @@
 
 NAME = libftprintf.a
 
-SRCS = ft_printf.c
+SRCS = ft_printf.c \
+		utils/ft_print_char.c \
+		utils/ft_print_string.c \
+		utils/ft_print_pointer.c \
+		utils/ft_print_number.c \
+		utils/ft_print_unsigned.c \
+		utils/ft_print_hexadecimal.c \
+		utils/ft_get_count_of_division.c \
 
 # SRCS_BONUS = .c
+
+LIBFT_PATH		=	libft
+LIBFT			=	$(LIBFT_PATH)/libft.a
 
 FLAGS = -Wall -Werror -Wextra
 
@@ -12,28 +22,34 @@ INCLUDE = ft_printf.h
 OBJS = ${SRCS:.c=.o}
 # OBJS_BONUS = $(SRCS_BONUS:.c=.o)
 
-all:	${NAME}
+all:	$(LIBFT) ${NAME}
 
 LIB = ar rcs
 CC	= gcc
 RM	= rm -f
 
+$(LIBFT):
+		make -C $(LIBFT_PATH)
+
 ${NAME}: ${OBJS} $(INCLUDE)
+	cp $(LIBFT) $(NAME)
 	 $(LIB) ${NAME} ${OBJS}
 
 %.o: %.c
-	$(CC) $(FLAGS) -c -o $@ $<
+	$(CC) $(CFLAGS) -c $< -o $(<:.c=.o) -I $(LIBFT_PATH)
 
 # bonus: $(OBJS) $(OBJS_BONUS)
 # 		$(LIB) $(NAME) $(OBJS) $(OBJS_BONUS)
 
 clean:
+	make -C $(LIBFT_PATH) clean
 	${RM} ${OBJS}
 # clean: ${RM} ${OBJS} ${OBJS_BONUS}
 
 fclean: clean
+	make -C $(LIBFT_PATH) fclean
 	${RM} ${NAME}
 
 re: fclean all
 
-.PHONY: clean, all, fclean, re, bonus
+.PHONY: clean, all, fclean, re, libft
--- ft_printf.c
+++ ft_printf.c
@@ -1,27 +1,60 @@
 #include "ft_printf.h"
 
-int	ft_printf(const char *str, ...)
+int	check_format(char *str, va_list args)
 {
-	va_list	args;
-	int		num;
+	int	num;
 
-	// int		i;
 	num = 0;
-	// i = 0;
-	va_start(args, str);
-	while (*str)
-	{
-		if (*str == '%')
-			str++;
-			
-		// num = 1;
-		else
-			// num = 2;
-			str++;
-	}
-	va_end(args);
+	if (*str == 'c')
+		num += ft_print_char(va_arg(args, int));
+	else if (*str == 's')
+		num += ft_print_string(va_arg(args, char *));
+	else if (*str == 'd' || *str == 'i')
+		num += ft_print_number(va_arg(args, int));
+	else if (*str == 'u')
+		num += ft_print_unsigned(va_arg(args, unsigned int));
+	else if (*str == 'x' || *str == 'X')
+		num += ft_print_hexadecimal(va_arg(args, unsigned int), str);
+	else if (*str == 'p')
+		num += ft_print_pointer(va_arg(args, unsigned long));
+	else
+		num += ft_print_char(*str);
 	return (num);
 }
 
-// Firstly, check if have in 1st paramentr %, and if 'yes',
-// check a type of the next element
+int	get_full_length(char *str, va_list args)
+{
+	int	num;
+
+	num = 0;
+	while (*str)
+	{
+		if (*str == '%')
+		{
+			str++;
+			num += check_format(str, args);
+		}
+		else
+		{
+			num += ft_print_char(*str);
+		}
+		str++;
+	}
+	return (num);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int		num;
+	char	*str;
+
+	if (!format || *format == '\0')
+		return (0);
+	str = ft_strdup(format);
+	va_start(args, format);
+	num = get_full_length(str, args);
+	va_end(args);
+	free(str);
+	return (num);
+}
